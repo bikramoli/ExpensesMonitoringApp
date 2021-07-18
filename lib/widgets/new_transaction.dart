0c55d75d7a095ab553f3f3ddedc8719bf42436cf
bikramoli <bikramoli75@gmail.com>
2021-07-18 17:50:02 +0545

    popup notification for successfully added transaction completed

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -5,17 +5,8 @@ class New_transaction extends StatelessWidget {
   final titleInput = TextEditingController();
   final amountInput = TextEditingController();
   final addNewT;
+
   New_transaction(this.addNewT);
-
-  void addT() {
-    final titleSet = titleInput.text;
-    final amountSet = double.parse(amountInput.text);
-
-    if (titleSet.isEmpty || amountSet <= 0) {
-      return;
-    }
-    addNewT(titleSet, amountSet);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +18,12 @@ class New_transaction extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                controller: titleInput,
-                onSubmitted: (_) => addT()),
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleInput,
+            ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountInput,
-              onSubmitted: (_) => addT(),
               keyboardType: TextInputType.number,
             ),
             FlatButton(
@@ -43,7 +33,16 @@ class New_transaction extends StatelessWidget {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () {
-                  addT();
+                  if (titleInput.text.isEmpty ||
+                      double.parse(amountInput.text) <= 0) {
+                    return;
+                  } else {
+                    addNewT(titleInput.text, double.parse(amountInput.text));
+                    //this is like popup notification
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content:
+                            Text('New transaction added successfully!!!!')));
+                  }
                 })
           ],
         ),
