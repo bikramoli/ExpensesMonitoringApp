997cdd1045afb5dde9e526a8ef21ae635a528de3
bikramoli <bikramoli75@gmail.com>
2021-07-20 11:51:29 +0545

    imported landing page in myapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,29 @@
 import 'package:expenses_monitor/widgets/landing.dart';
-
+import 'package:expenses_monitor/widgets/transaction_list.dart';
+import './models/transaction.dart';
 import './models/chart.dart';
 import 'package:intl/intl.dart';
 import './widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 import './widgets/chart.dart';
 import './widgets/landing.dart';
+import './widgets/new_transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Expenses Monitoring',
-      home: MyHomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _MyAppState extends State<MyApp> {
   var page;
 
   void expensesE() {
     setState(() {
-      page = 'transaction_page';
+      page = 2;
     });
   }
 
@@ -42,7 +33,69 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void reset() {
     setState(() {
-      page = 'landing_page';
+      page = 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Expenses Monitoring',
+        home: Scaffold(
+            body: page == 2
+                ? MyHomePage()
+                : page == 1
+                    ? Landing(
+                        expenses: expensesE,
+                        earning: earningE,
+                      )
+                    : Text('this')));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Chart> _chart = [
+    Chart(date: DateTime.now(), cal: '63%'),
+    Chart(date: DateTime.now(), cal: '90%'),
+    Chart(date: DateTime.now(), cal: '80%')
+  ];
+  final List<Transaction> _transaction = [
+    Transaction(
+        id: 't1', title: 'New shoes', amount: 50.21, date: DateTime.now()),
+  ];
+
+  void addNewTransaction(String txTitle, double txAmount) {
+    final addTx = Transaction(
+        id: DateTime.now(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+
+    setState(() {
+      _transaction.add(addTx);
+    });
+  }
+
+  void startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return New_transaction(addNewTransaction);
+      },
+    );
+  }
+
+  var page;
+
+  void reset() {
+    setState(() {
+      page = 1;
     });
   }
 
@@ -60,26 +113,33 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: reset,
           ),
         ),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () => startAddNewTransaction(context),
+              icon: Icon(Icons.add))
+        ],
       ),
       body: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/landing.jpg'), fit: BoxFit.cover)),
-        child: Column(
+        child: ListView(
           //mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            page == 'landing_page'
-                ? Landing(
-                    expenses: expensesE,
-                    earning: earningE,
-                  )
-                : page == 'transaction_page'
-                    ? UserTransaction()
-                    : Text('this')
+            Column(
+              children: [
+                Charts(_chart),
+                TransactionList(_transaction),
+              ],
+            )
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => startAddNewTransaction(context),
+        child: Icon(Icons.add),
       ),
     );
   }
